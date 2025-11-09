8fc6be49ee7b94d51949734db9dbfb243d2cbd73
farwa234 <areh12378@gmail.com>
2025-11-10 02:49:28 +0500

    done

---
+++ Chapter-no-43to48/app.js
@@ -0,0 +1,58 @@
+// --------------Chapter no 43 to 48 Start ----------------------
+
+
+// PLEASE CHECK INDEX FILE 
+
+
+
+//Q 1. Show an alert box on click on a link.?
+
+// const showAlert = ()=>{
+//   alert("Hello! You clicked the link.")
+// }
+
+//Q 2. Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.?
+
+// let thanks=()=>{
+//   alert(`Thanks for purchasing from us `)
+// }
+
+
+//Q 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted.?
+
+// const remove=(e)=>{
+// e.parentNode.parentNode.remove()
+// }
+
+
+// 4. Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+
+
+// Answer is index-file
+
+
+// Q5. Show a counter in browser. Counter should increase on click on increase
+// button and decrease on click on decrease button. And show updated counter
+// value in browser.?
+
+// const increase= ()=>{
+//     let value = document.getElementById("counter").innerHTML
+//      value = parseInt(value)
+//      console.log(value)
+//      value = value + 1
+//      document.getElementById("counter").innerHTML = value
+
+// }
+
+// const decrease=()=>{
+//     let value = document.getElementById("counter").innerHTML
+//      value = parseInt(value)
+//      console.log(value)
+//      value = value -1
+//      document.getElementById("counter").innerHTML = value
+
+// }
